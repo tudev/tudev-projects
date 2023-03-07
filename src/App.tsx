71b1c3c98960d790b2a4c25170838620395b67da
Ryan Hodge <ryanhodge240@gmail.com>
2023-03-06 21:50:45 -0500

    Routing is working properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { HashRouter as Router, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <p>TUDev test</p>
+      	<Router>
+			<div className='App'>
+				<Link to={'/home'}>Home</Link>
+				<Link to={'/events'}>Events</Link>
+				<div className='content'>
+					{/* Define routes within the website */}
+					<Routes>
+						<Route path='/' element={<p>Home</p>}/>
+						<Route path={'/home'} element={<p>Home2</p>}/>
+						<Route path={'/events'} element={<p>Events2</p>}/>
+					</Routes>
+				</div>
+			</div>
+		</Router>
     </div>
   );
 }
