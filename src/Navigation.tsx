ec6a0f7f63feafa320618eb5b1212012c0c8d659
Ryan Hodge <ryanhodge240@gmail.com>
2023-03-06 22:30:07 -0500

    Creating navigation bar

---
+++ src/Navigation.tsx
@@ -0,0 +1,16 @@
+// Third party imports
+import React from 'react'
+
+// Custom styles
+import Styles from './NavigationStyles'
+
+
+const RyanHodge = (): React.ReactElement => {
+	return (
+		<Styles.NavigationContainer>
+            <p>Navigation</p>
+		</Styles.NavigationContainer>
+	)
+}
+
+export default RyanHodge
---
+++ src/NavigationStyles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export default class NavigationStyles {
+
+	static readonly NavigationContainer = styled.div`
+        background-color: #a20000;
+        position: fixed;
+        top: 0;
+        width: 100%;
+        z-index: 100;
+	`
+
+}
